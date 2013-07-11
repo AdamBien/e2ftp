7e3ddd79ed33ef0bebc1dd6266a1015399c0fa93
Adam Bien <abien@adam-bien.com>
2013-07-11 07:23:18 +0200

    Sending, receiving and listing is tested

--- ftpserver-st/src/test/java/org/eftp/ftpserver/st/FTPServerWrapperIT.java
+++ ftpserver-st/src/test/java/org/eftp/ftpserver/st/FTPServerWrapperIT.java
@@ -1,7 +1,16 @@
 package org.eftp.ftpserver.st;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
+import static org.hamcrest.CoreMatchers.is;
+import org.junit.After;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -19,16 +28,51 @@ public class FTPServerWrapperIT {
     }
 
     @Test
-    public void connect() throws IOException {
+    public void connectAndDisconnect() throws IOException {
         this.client.connect(HOST, PORT);
         this.client.disconnect();
     }
 
     @Test
     public void login() throws IOException {
+        connect();
+    }
+
+    @Test
+    public void list() throws IOException {
+        connect();
+        FTPFile[] listDirectories = this.client.listDirectories();
+        assertNotNull(listDirectories);
+        assertTrue(listDirectories.length > 0);
+    }
+
+    @Test
+    public void sendAndReceive() throws IOException {
+        connect();
+        String remoteFile = "e2ftp-client" + System.currentTimeMillis() + ".txt";
+        final String expected = "just a test";
+        ByteArrayInputStream bais = new ByteArrayInputStream(expected.getBytes());
+        this.client.storeFile(remoteFile, bais);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        this.client.retrieveFile(remoteFile, baos);
+        byte[] actual = baos.toByteArray();
+        assertNotNull(actual);
+        assertThat(new String(actual), is(expected));
+        this.client.deleteFile(remoteFile);
+
+        baos = new ByteArrayOutputStream();
+        boolean fileExists = this.client.retrieveFile(remoteFile, baos);
+        assertFalse(fileExists);
+    }
+
+    @After
+    public void cleanup() throws IOException {
+        this.client.disconnect();
+    }
+
+    private void connect() throws IOException {
         this.client.connect(HOST, PORT);
         this.client.login(USER, PASSWORD);
-        this.client.disconnect();
     }
 
 }

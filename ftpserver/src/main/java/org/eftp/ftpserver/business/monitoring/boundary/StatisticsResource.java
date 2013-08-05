339de0dcf94d60fcdd93890a7dbe1de587390ddf
Adam Bien <abien@adam-bien.com>
2013-08-05 04:32:19 +0200

    Statistics completed (https://github.com/AdamBien/e2ftp/issues/4)

--- ftpserver/src/main/java/org/eftp/ftpserver/business/monitoring/boundary/StatisticsResource.java
+++ ftpserver/src/main/java/org/eftp/ftpserver/business/monitoring/boundary/StatisticsResource.java
@@ -2,6 +2,9 @@ package org.eftp.ftpserver.business.monitoring.boundary;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import org.apache.ftpserver.ftplet.FtpStatistics;
@@ -18,7 +21,23 @@ public class StatisticsResource {
     FtpStatistics statistics;
 
     @GET
-    public String all() {
-        return this.statistics.toString();
+    public JsonObject all() {
+        JsonObjectBuilder builder = Json.createObjectBuilder();
+        builder.add("currentAnonymousLoginNumber", this.statistics.getCurrentAnonymousLoginNumber());
+        builder.add("currentConnectionNumber", this.statistics.getCurrentConnectionNumber());
+        builder.add("currentLoginNumber", this.statistics.getCurrentLoginNumber());
+        builder.add("totalAnonymousLoginNumber", this.statistics.getTotalAnonymousLoginNumber());
+        builder.add("totalConnectionNumber", this.statistics.getTotalConnectionNumber());
+        builder.add("totalDeleteNumber", this.statistics.getTotalDeleteNumber());
+        builder.add("totalDirectoryCreated", this.statistics.getTotalDirectoryCreated());
+        builder.add("totalDirectoryRemoved", this.statistics.getTotalDirectoryRemoved());
+        builder.add("totalDownloadNumber", this.statistics.getTotalDownloadNumber());
+        builder.add("totalDownloadSize", this.statistics.getTotalDownloadSize());
+        builder.add("totalFailedLoginNumber", this.statistics.getTotalFailedLoginNumber());
+        builder.add("totalLoginNumber", this.statistics.getTotalLoginNumber());
+        builder.add("totalUploadNumber", this.statistics.getTotalUploadNumber());
+        builder.add("totalUploadSize", this.statistics.getTotalUploadSize());
+        builder.add("startTime", this.statistics.getStartTime().toString());
+        return builder.build();
     }
 }

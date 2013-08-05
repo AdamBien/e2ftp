ed7437e82f3029971999673a965d3a9ecd624a80
Adam Bien <abien@adam-bien.com>
2013-08-05 03:57:48 +0200

    Formatting

--- ftpserver/src/main/java/org/eftp/ftpserver/business/boot/control/ManagedFtpServerContext.java
+++ ftpserver/src/main/java/org/eftp/ftpserver/business/boot/control/ManagedFtpServerContext.java
@@ -34,96 +34,97 @@ import org.eftp.ftpserver.business.users.control.InMemoryUserManager;
 @Singleton
 @ConcurrencyManagement(ConcurrencyManagementType.BEAN)
 public class ManagedFtpServerContext implements FtpServerContext {
-
+    
     int corePoolSize = 2;
     int maximumPoolSize = 8;
     long keepAliveTimeInHours = 1;
-
+    
     BlockingQueue<Runnable> workQueue;
-
+    
     @Resource
     ManagedThreadFactory threadFactory;
-
+    
     private DefaultFtpServerContext delegate;
     private ThreadPoolExecutor executor;
-
+    
     @PostConstruct
     public void init() {
         this.workQueue = new LinkedBlockingQueue<>();
         this.delegate = new DefaultFtpServerContext();
         //Problems with passing ThreadFactory
         this.executor = new ThreadPoolExecutor(corePoolSize, maximumPoolSize, keepAliveTimeInHours, TimeUnit.HOURS, workQueue);
+        this.delegate.setFileSystemManager(null);
     }
-
+    
     @Override
     public ConnectionConfig getConnectionConfig() {
         return delegate.getConnectionConfig();
     }
-
+    
     @Override
     public MessageResource getMessageResource() {
         return delegate.getMessageResource();
     }
-
+    
     @Override
     public FtpletContainer getFtpletContainer() {
         return delegate.getFtpletContainer();
     }
-
+    
     @Override
     public Listener getListener(String string) {
         return delegate.getListener(string);
     }
-
+    
     @Override
     public Map<String, Listener> getListeners() {
         return delegate.getListeners();
     }
-
+    
     @Override
     public CommandFactory getCommandFactory() {
         return delegate.getCommandFactory();
     }
-
+    
     @Override
     public void dispose() {
         delegate.dispose();
     }
-
+    
     @Override
     public UserManager getUserManager() {
         return delegate.getUserManager();
     }
-
+    
     @Override
     public FileSystemFactory getFileSystemManager() {
         return delegate.getFileSystemManager();
     }
-
+    
     @Override
     @Produces
     public FtpStatistics getFtpStatistics() {
         return delegate.getFtpStatistics();
     }
-
+    
     @Override
     public Ftplet getFtplet(String string) {
         return delegate.getFtplet(string);
     }
-
+    
     @Override
     public ThreadPoolExecutor getThreadPoolExecutor() {
         return this.executor;
     }
-
+    
     public void setConnectionConfig(ConnectionConfig createConnectionConfig) {
         this.delegate.setConnectionConfig(createConnectionConfig);
     }
-
+    
     public void setUserManager(InMemoryUserManager userManager) {
         this.delegate.setUserManager(userManager);
     }
-
+    
     public void addListener(String adefault, Listener createListener) {
         this.delegate.addListener(adefault, createListener);
     }

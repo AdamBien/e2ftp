a21e6da6c18dadd41ef7f380fee39de2991eb2bd
Adam Bien <abien@adam-bien.com>
2013-08-14 19:38:51 +0200

    RESTSupport implemented

--- ftpserver-st/src/test/java/org/eftp/ftpserver/business/configuration/ConfigurationResourceIT.java
+++ ftpserver-st/src/test/java/org/eftp/ftpserver/business/configuration/ConfigurationResourceIT.java
@@ -6,12 +6,10 @@ package org.eftp.ftpserver.business.configuration;
 import javax.json.Json;
 import javax.json.JsonArray;
 import javax.json.JsonObject;
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
-import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import org.eftp.ftpserver.business.RESTSupport;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
@@ -22,41 +20,38 @@ import org.junit.Test;
  *
  * @author adam-bien.com
  */
-public class ConfigurationResourceIT {
+public class ConfigurationResourceIT extends RESTSupport {
 
-    private Client client;
     private static final String CONFIGURATION_URI = "http://localhost:8080/ftpserver/api/configuration";
-    private WebTarget configurationTarget;
 
     @Before
     public void init() {
-        this.client = ClientBuilder.newClient();
-        this.configurationTarget = this.client.target(CONFIGURATION_URI);
+        super.init(CONFIGURATION_URI);
     }
 
     @Test
     public void crud() {
-        JsonArray allEntries = this.configurationTarget.request(MediaType.APPLICATION_JSON).get(JsonArray.class);
+        JsonArray allEntries = this.mainTarget.request(MediaType.APPLICATION_JSON).get(JsonArray.class);
         assertFalse(allEntries.isEmpty());
         System.out.println("-- " + allEntries);
         String name = "hugo " + System.currentTimeMillis();
         //creation
         JsonObject newEntry = Json.createObjectBuilder().add("name", name).add("value", 42).build();
-        Response putResponse = this.configurationTarget.request().put(Entity.entity(newEntry, MediaType.APPLICATION_JSON));
+        Response putResponse = this.mainTarget.request().put(Entity.entity(newEntry, MediaType.APPLICATION_JSON));
         assertThat(putResponse.getStatus(), is(204));
 
         //fetch
-        Response fetchResponse = this.configurationTarget.path(name).request(MediaType.APPLICATION_JSON).get(Response.class);
+        Response fetchResponse = this.mainTarget.path(name).request(MediaType.APPLICATION_JSON).get(Response.class);
         assertThat(fetchResponse.getStatus(), is(200));
         JsonObject value = fetchResponse.readEntity(JsonObject.class);
         assertThat(value.getString("value"), is("42"));
 
         //removal
-        Response deleteResponse = this.configurationTarget.path(name).request(MediaType.APPLICATION_JSON).delete();
+        Response deleteResponse = this.mainTarget.path(name).request(MediaType.APPLICATION_JSON).delete();
         assertThat(deleteResponse.getStatus(), is(204));
 
         //empty fetch
-        Response emptyFetchResponse = this.configurationTarget.path(name).request(MediaType.APPLICATION_JSON).get(Response.class);
+        Response emptyFetchResponse = this.mainTarget.path(name).request(MediaType.APPLICATION_JSON).get(Response.class);
         assertThat(emptyFetchResponse.getStatus(), is(204));
 
     }

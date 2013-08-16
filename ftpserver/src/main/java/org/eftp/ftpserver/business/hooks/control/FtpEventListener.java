5a5de7f25c3cc1539a4ce66cd65e083da81aa5e0
Adam Bien <abien@adam-bien.com>
2013-08-16 12:00:26 +0200

    Everything listeners receive events as well now

--- ftpserver/src/main/java/org/eftp/ftpserver/business/hooks/control/FtpEventListener.java
+++ ftpserver/src/main/java/org/eftp/ftpserver/business/hooks/control/FtpEventListener.java
@@ -3,12 +3,14 @@
  */
 package org.eftp.ftpserver.business.hooks.control;
 
+import java.util.ArrayList;
 import java.util.List;
 import javax.ejb.Stateless;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 import org.eftp.events.Command;
 import org.eftp.events.FtpEvent;
+import org.eftp.events.FtpEventName;
 import org.eftp.ftpserver.business.hooks.boundary.HooksRegistry;
 import org.eftp.ftpserver.business.hooks.entity.Hook;
 
@@ -26,8 +28,12 @@ public class FtpEventListener {
     HooksRegistry registry;
 
     public void onFtpEventArrival(@Observes @Command FtpEvent event) {
+        List<Hook> allListeners = new ArrayList<>();
+        List<Hook> everythingListeners = registry.findByCommand(FtpEventName.EVERYTHING);
         List<Hook> hooks = registry.findByCommand(event.getCommand());
-        for (Hook hook : hooks) {
+        allListeners.addAll(everythingListeners);
+        allListeners.addAll(hooks);
+        for (Hook hook : allListeners) {
             this.hookInvoker.callback(hook, event);
         }
     }

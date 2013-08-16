50f8f0f66aeea1be0845198bc1cd825d48a3a60a
Adam Bien <abien@adam-bien.com>
2013-08-16 10:33:05 +0200

    Extracted from annotation

---
+++ ftpservice-api/src/main/java/org/eftp/events/FtpEventName.java
@@ -0,0 +1,23 @@
+/*
+ *
+ */
+package org.eftp.events;
+
+/**
+ *
+ * @author adam-bien.com
+ */
+public enum FtpEventName {
+
+    LOGIN, SITE,
+    RENAME_END, RENAME_START,
+    UPLOAD_UNIQUE_START, UPLOAD_UNIQUE_END,
+    APPEND_START, APPEND_END,
+    MKDIR_START, MKDIR_END,
+    RMDIR_START, RMDIR_END,
+    DOWNLOAD_START, DOWNLOAD_END,
+    UPLOAD_START, UPLOAD_END,
+    DELETE_END, DELETE_START,
+    DISCONNECT, CONNECT,
+    EVERYTHING;
+}
